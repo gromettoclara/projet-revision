6967951959084d454b9cc9590e68f05a77e4c9a9
Clara <gromettoclara@gmail.com>
2025-11-04 18:40:42 +0100

    rajout d'articles et maquettage

---
+++ auteur/script_collatex.py
@@ -0,0 +1,16 @@
+from collatex import *
+
+with open('witnesses/V1_16_09_24.txt') as f:
+    premiere_version = " ".join(f.readlines())
+with open('witnesses/V2_14_11_24.txt') as f:
+    derniere_relecture = " ".join(f.readlines())
+
+collation = Collation()
+collation.add_plain_witness("V1", premiere_version)
+collation.add_plain_witness("V2", derniere_relecture)
+
+
+tei_output = collate(collation, output="tei-xml", segmentation=False, near_match=False)
+
+with open("collation.xml", "w") as output:
+    output.write(tei_output.replace("<app>", "\n<app>"))
